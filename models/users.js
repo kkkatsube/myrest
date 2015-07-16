8af8f50aac2c2493ae90683f3da39cfea202d95e
katsube kimihiko <katsube-kimihiko@dmm.com>
2015-07-16 12:21:29 +0900

    JsDoc修正

--- models/users.js
+++ models/users.js
@@ -43,7 +43,8 @@ Users.prototype.create = function( name, func ) {
 /**
  * 指定IDのUsersモデルの内容を、指定の内容で更新し、永続化する
  * @method update
- * @param {String} name 生成するUsersモデルの名前
+ * @param {Integer} id 更新するUsersモデルのID
+ * @param {String} name 更新するUsersモデルの名前
  * @param {Function} func callback関数 callback(err,rows)
  */
 Users.prototype.update = function( id, name, func ) {
@@ -52,8 +53,9 @@ Users.prototype.update = function( id, name, func ) {
 
 /**
  * 指定IDのUsersモデルを削除する
- * @method 
- * @param 
+ * @method delete
+ * @param {Integer} id 削除するUsersモデルのID
+ * @param {Function} func callback関数 callback(err,rows)
  */
 Users.prototype.delete = function( id, func ) {
     mysql.query('delete from users where id = ?', [ id ], func);
